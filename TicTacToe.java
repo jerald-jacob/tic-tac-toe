d44f63a734b94e576b9e75a160814b28eb514162
Jerald Jacob <jeraldjacob10@gmail.com>
2020-05-05 18:14:38 +0530

    Game updated with two conditions

--- TicTacToe.java
+++ TicTacToe.java
@@ -10,17 +10,17 @@ public class TicTacToe  {
 }
 
 class GamePlay {
+	String line=null;
+	int userInput=0;
 	ArrayList<String> gameBord = new ArrayList<String>(Arrays.asList(" "," "," "," "," "," "," "," "," ") );
 	Set<Integer> entry_dtls = new HashSet<Integer>(); 
  	void myMethod() {
    		Scanner myObj = new Scanner(System.in);  
 
 		System.out.println("Enter Position");
-		int userInput=0;
-		while(entry_dtls.size()<9){
-			display();
-
 		
+		while(userInput<9){
+			display();
 
 		try {
 				Integer playerInput = myObj.nextInt(); 
@@ -36,7 +36,7 @@ class GamePlay {
 				System.out.println("Invalid input; re-enter slot number:");
 				continue;
 			}
-
+userInput+=1;
 		}
 	}
 
@@ -53,11 +53,61 @@ class GamePlay {
   	entry_dtls.add(playerInput);
 	display();
 	System.out.println(entry_dtls);
+
   }
   void display(){
   	System.out.println("-------\n|"+(gameBord.get(0))+"|"+(gameBord.get(1))+"|"+(gameBord.get(2))+"|"+"\t  \t"+"0 1 2");
 			System.out.println("-------\n|"+(gameBord.get(3))+"|"+(gameBord.get(4))+"|"+(gameBord.get(5))+"|"+"\t"+"-->"+"\t"+"3 4 5");
 			System.out.println("-------\n|"+(gameBord.get(6))+"|"+(gameBord.get(7))+"|"+(gameBord.get(8))+"|"+"\t  \t"+"6 7 8");
 			System.out.println("-------");
+			checkWinOrLoast();
+  }
+  void checkWinOrLoast(){
+  	for (int a = 0; a < 8; a++) {
+			String line = null;
+			switch (a) {
+			case 0:
+				line = gameBord.get(0) + gameBord.get(1) + gameBord.get(2);
+				break;
+			case 1:
+				line = gameBord.get(3) + gameBord.get(4) + gameBord.get(5);
+				break;
+			case 2:
+				line = gameBord.get(6) + gameBord.get(7) + gameBord.get(8);
+				break;
+			case 3:
+				line = gameBord.get(0) + gameBord.get(3) + gameBord.get(6);
+				break;
+			case 4:
+				line = gameBord.get(1) + gameBord.get(4) + gameBord.get(7);
+				break;
+			case 5:
+				line = gameBord.get(2) + gameBord.get(5) + gameBord.get(8);
+				break;
+			case 6:
+				line = gameBord.get(0) + gameBord.get(4) + gameBord.get(8);
+				break;
+			case 7:
+				line = gameBord.get(2) + gameBord.get(4) + gameBord.get(6);
+				break;
+			}
+			if (line.equals("XXX")) {
+				System.out.println("Player one win");
+				//return "X";
+				userInput+=9;
+
+			} else if (line.equals("OOO")) {
+				System.out.println("Player two win");
+				userInput+=9;
+				//return "O";
+			}
+
+		}
+if (entry_dtls.size()==9 && (line!=("XXX"))) 
+				{
+					System.out.println("draw");
+						userInput+=9;
+}
+
   }
 }
